b816e70f9661836a3ad089316b4bf3c22166e45e
Sential <sentialx@gmail.com>
2019-08-15 18:27:24 +0200

    feat: save electron version to file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { promisify } from 'util';
 import { join } from 'path';
 import { promises } from 'fs';
 import * as rmrf from 'rimraf';
+import * as semver from 'semver';
 
 import { DEFAULT_EXCLUDE } from './constants';
 
@@ -16,12 +17,31 @@ const copy = promisify(ncp);
 export const build = async (baseDir: string, dest: string): Promise<void> => {
   try {
     const resources = join(dest, 'resources');
+    const operations: Promise<any>[] = [];
+
+    const pkg = JSON.parse(
+      await promises.readFile(join(baseDir, 'package.json'), 'utf8'),
+    );
+
+    let electronVersion: string;
+
+    if (pkg) {
+      if (pkg.devDependencies && pkg.devDependencies.electron) {
+        electronVersion = semver.clean(pkg.devDependencies.electron);
+      } else {
+        throw new Error('Electron should be installed as a dev dependency.');
+      }
+    } else {
+      throw new Error(
+        'package.json file does not exist in the specified directory.',
+      );
+    }
+
+    await promises.writeFile(join(dest, 'electron_version'), electronVersion);
 
     const files = await promises.readdir(baseDir);
 
-    mkdirp(resources);
-
-    const operations: Promise<any>[] = [];
+    await mkdirp(resources);
 
     for (const file of files) {
       if (DEFAULT_EXCLUDE.includes(file) || file === dest) continue;
@@ -34,6 +54,6 @@ export const build = async (baseDir: string, dest: string): Promise<void> => {
     await asar.createPackage(resources, join(dest, 'app.asar'));
     await rimraf(resources);
   } catch (e) {
-    console.error(e);
+    console.log(e);
   }
 };
