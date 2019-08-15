598e43e1570e708063afcd6a5f21caa003cf2915
Sential <sentialx@gmail.com>
2019-08-15 18:01:21 +0200

    feat: add --projectDir, --project option

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,14 +4,19 @@ import * as program from 'commander';
 import { build } from '..';
 import { DEFAULT_DEST } from '../constants';
 
-program.option('-m, --mac', 'Build for macOS');
+program.option('-m, --mac', 'Build for macOS.');
+program.option(
+  '--projectDir, --project',
+  'The path to project directory. Defaults to current working directory.',
+);
 
 program.parse(process.argv);
 
-const basePath = process.cwd();
-
 (async function(): Promise<void> {
   if (program.mac) {
-    build(basePath, DEFAULT_DEST);
+    build(
+      program.projectDir ? program.projectDir : process.cwd(),
+      DEFAULT_DEST,
+    );
   }
 })();
