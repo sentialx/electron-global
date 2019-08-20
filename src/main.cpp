a8c0d306fca3cce70eb3f04afcd924f65b96717b
sentialx <sentialx@gmail.com>
2019-08-20 18:38:05 +0200

    fix: remove unused includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,5 @@
-#include <errno.h>
 #include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <unistd.h>
 #include <algorithm>
 #include <experimental/filesystem>
 #include <fstream>
